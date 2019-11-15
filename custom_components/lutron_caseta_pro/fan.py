8fed645cce754a2706551502e184613cdb9a8837
jreed509 <jreed509@me.com>
2019-11-14 21:26:48 -0500

    Updated fan speed mappings to match Lutron's
    These speed mappings are based off of the values used by Lutron in the telnet interface. The update state function was updated to accommodate float values instead of integers.

--- custom_components/lutron_caseta_pro/fan.py
+++ custom_components/lutron_caseta_pro/fan.py
@@ -18,11 +18,11 @@ _LOGGER = logging.getLogger(__name__)
 
 SPEED_MEDIUM_HIGH = 'medium_high'
 SPEED_MAPPING = {
-    SPEED_OFF: 0,
-    SPEED_LOW: 25,
-    SPEED_MEDIUM: 50,
-    SPEED_MEDIUM_HIGH: 75,
-    SPEED_HIGH: 100
+    SPEED_OFF: 0.00,
+    SPEED_LOW: 25.10,
+    SPEED_MEDIUM: 50.20,
+    SPEED_MEDIUM_HIGH: 75.30,
+    SPEED_HIGH: 100.00
 }
 
 
@@ -190,15 +190,13 @@ class CasetaFan(FanEntity):
     def update_state(self, value):
         """Update internal state and fan speed."""
         self._is_on = value > SPEED_MAPPING[SPEED_OFF]
-        if value in range(SPEED_MAPPING[SPEED_MEDIUM_HIGH] + 1, SPEED_MAPPING[SPEED_HIGH] + 1):
+        if value > SPEED_MAPPING[SPEED_MEDIUM_HIGH] and value <= SPEED_MAPPING[SPEED_HIGH]:
             self._speed = SPEED_HIGH
-        elif value in range(SPEED_MAPPING[SPEED_MEDIUM] + 1, SPEED_MAPPING[SPEED_MEDIUM_HIGH] + 1):
-            # 51% - 55% are missing from Lutron integration protocol
-            # we will treat as medium_high
+        elif value > SPEED_MAPPING[SPEED_MEDIUM] and value <= SPEED_MAPPING[SPEED_MEDIUM_HIGH]:
             self._speed = SPEED_MEDIUM_HIGH
-        elif value in range(SPEED_MAPPING[SPEED_LOW] + 1, SPEED_MAPPING[SPEED_MEDIUM] + 1):
+        elif value > SPEED_MAPPING[SPEED_LOW] and value <= SPEED_MAPPING[SPEED_MEDIUM]:
             self._speed = SPEED_MEDIUM
-        elif value in range(SPEED_MAPPING[SPEED_OFF] + 1, SPEED_MAPPING[SPEED_LOW] + 1):
+        elif value > SPEED_MAPPING[SPEED_OFF] and value <= SPEED_MAPPING[SPEED_LOW]:
             self._speed = SPEED_LOW
         elif value == SPEED_MAPPING[SPEED_OFF]:
             self._speed = SPEED_OFF
